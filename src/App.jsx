42764bfbeba15f08b64bfcdf810d7b6497128672
Pedro Henrique Spelta <80366307+PedroSpelta@users.noreply.github.com>
2021-07-22 17:45:14 -0300

    making a new api consume

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./styles.css";
 import pokemons from "./data";
 import "./pokemon.css";
+import { useState } from "react";
 
 function Pokemon(props) {
   const { name, type, averageWeight, image } = props.pokemon;
@@ -22,18 +23,32 @@ function Pokemon(props) {
 
 function Pokedex(props) {
   const { pokemons } = props;
+  // const [poke, setPoke] = useState([]);
   return pokemons.map((pokemon) => (
     <Pokemon key={pokemon.id} pokemon={pokemon} />
   ));
 }
 
+function PokeApi() {
+  const [pokeList, setPokeList] = useState([]);
+  async function pokemonFetch() {
+    // const baseUrl = 'https://pokeapi.co/api/v2/';
+    const pokeResponse = await fetch(
+      "https://pokeapi.co/api/v2/pokedex/original-johto"
+    );
+    const pokeData = (await pokeResponse).json().then((data) => {
+      data.pokemon_entries.forEach((pokemon) => {});
+    });
+  }
+  pokemonFetch();
+  return <h1>teste</h1>;
+}
+
 export default function App() {
   return (
-    <div className="App">
-      <div className="pokedex-container">
-        <Pokedex pokemons={pokemons} />
-      </div>
-      {/* <Pokemon poke={pokemons[0]} /> */}
+    <div className="pokedex-container">
+      <Pokedex pokemons={pokemons} />
+      <PokeApi />
     </div>
   );
 }
