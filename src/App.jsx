ba51b68ff8a2374070113f26ca754cee35fe9832
PedroSpelta <p.h.s.8989@hotmail.com>
2021-07-22 23:24:26 -0300

    todos pokemons carregando com gif da api

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,60 @@
-import "./styles.css";
-import pokemons from "./data";
-import "./pokemon.css";
-import { useState } from "react";
+import './styles.css';
+import './pokemon.css';
+import { useState, useEffect } from 'react';
 
 function Pokemon(props) {
-  const { name, type, averageWeight, image } = props.pokemon;
+  const { name } = props;
+  const [pokeInfo, setPokeInfo] = useState({});
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then((res) => res.json())
+      .then((json) => {
+        const gif = json.sprites.versions.['generation-v'].['black-white'].animated.front_default;
+        // const spirte =
+        setPokeInfo({
+          id: json.id,
+          sprite:
+            gif,
+          // json.sprites.versions.['generation-v'].['black-white'].animated.front_default,
+            type: json.types[0].type.name,
+          weight: json.weight
+        });
+      });
+  });
   return (
     <div className="pokemon-container">
       <div className="pokemon-info">
         <p>{name}</p>
-        <p>{type}</p>
+        <p>{pokeInfo.type}</p>
         <p>
-          Avg weight: {averageWeight.value} {averageWeight.measurementUnit}
+          Avg weight: {pokeInfo.weight}
         </p>
       </div>
       <div className="pokemon-gif">
-        <img src={image} alt={`${name} animated gif`} />
+        <img src={pokeInfo.sprite} alt={`${name} animated gif`} />
       </div>
     </div>
   );
 }
 
-function Pokedex(props) {
-  const { pokemons } = props;
-  // const [poke, setPoke] = useState([]);
-  return pokemons.map((pokemon) => (
-    <Pokemon key={pokemon.id} pokemon={pokemon} />
-  ));
-}
-
-function PokeApi() {
+function Pokedex() {
   const [pokeList, setPokeList] = useState([]);
-  async function pokemonFetch() {
-    // const baseUrl = 'https://pokeapi.co/api/v2/';
-    const pokeResponse = await fetch(
-      "https://pokeapi.co/api/v2/pokedex/original-johto"
-    );
-    const pokeData = (await pokeResponse).json().then((data) => {
-      data.pokemon_entries.forEach((pokemon) => {});
-    });
-  }
-  pokemonFetch();
-  return <h1>teste</h1>;
+  const [audio] = useState(new Audio('/backgroundmsc.mp3'))
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokedex/original-johto')
+      .then((res) => res.json())
+      .then((json) => {
+        setPokeList(json.pokemon_entries);
+      });
+    audio.play();
+  });
+  return pokeList.map((pokemon) => <Pokemon name={pokemon.pokemon_species.name} key={pokemon.pokemon_species.name} />);
 }
 
 export default function App() {
   return (
     <div className="pokedex-container">
-      <Pokedex pokemons={pokemons} />
-      <PokeApi />
+      <Pokedex />
     </div>
   );
 }
