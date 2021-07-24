83e39665d6411e6db6a310bd637f7b41e87edfce
PedroSpelta <p.h.s.8989@hotmail.com>
2021-07-24 10:41:05 -0300

    imagem de fundo de cada card adicionada

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,44 @@
 import './styles.css';
 import './pokemon.css';
 import { useState, useEffect } from 'react';
+import pokemons from './data';
+import element from './elements';
+import pokeball from './images/pokeball.png';
 
 function Pokemon(props) {
   const { name } = props;
   const [pokeInfo, setPokeInfo] = useState({});
+  const [isActive, setIsActive] = useState(true);
   useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-      .then((res) => res.json())
-      .then((json) => {
-        const gif = json.sprites.versions.['generation-v'].['black-white'].animated.front_default;
-        // const spirte =
-        setPokeInfo({
-          id: json.id,
-          sprite:
-            gif,
-          // json.sprites.versions.['generation-v'].['black-white'].animated.front_default,
+    if (isActive) {
+      setIsActive(false);
+      fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        .then((res) => res.json())
+        .then((json) => {
+          const gif =
+            json.sprites.versions['generation-v']['black-white'].animated
+              .front_default;
+          // const spirte =
+          // const local = localStorage.setItem()
+          setPokeInfo({
+            id: json.id,
+            sprite: gif,
             type: json.types[0].type.name,
-          weight: json.weight
+            weight: json.weight,
+          });
         });
-      });
+    }
   });
   return (
-    <div className="pokemon-container">
+    <div
+      className="pokemon-container"
+      style={{ backgroundColor: element[pokeInfo.type] }}
+    >
+      <img className="poke-ball" src={pokeball} alt="test" />
       <div className="pokemon-info">
         <p>{name}</p>
         <p>{pokeInfo.type}</p>
-        <p>
-          Avg weight: {pokeInfo.weight}
-        </p>
+        <p>Avg weight: {pokeInfo.weight}</p>
       </div>
       <div className="pokemon-gif">
         <img src={pokeInfo.sprite} alt={`${name} animated gif`} />
@@ -38,17 +48,19 @@ function Pokemon(props) {
 }
 
 function Pokedex() {
-  const [pokeList, setPokeList] = useState([]);
-  const [audio] = useState(new Audio('/backgroundmsc.mp3'))
+  // const [pokeList, setPokeList] = useState([]);
+  // const [audio] = useState(new Audio('/backgroundmsc.mp3'))
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokedex/original-johto')
-      .then((res) => res.json())
-      .then((json) => {
-        setPokeList(json.pokemon_entries);
-      });
-    audio.play();
+    // audio.play();
   });
-  return pokeList.map((pokemon) => <Pokemon name={pokemon.pokemon_species.name} key={pokemon.pokemon_species.name} />);
+  return pokemons.map((pokemon) => (
+    <div>
+      <Pokemon
+        name={pokemon.pokemon_species.name}
+        key={pokemon.pokemon_species.name}
+      />
+    </div>
+  ));
 }
 
 export default function App() {
---
+++ src/elements.js
@@ -0,0 +1,7 @@
+const element = {
+  fire: '#FB6C6C',
+  grass: '#48D0B0',
+  water: '#76BDFE',
+  electric: '#FFD86F'
+}
+export default element;
