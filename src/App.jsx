a16f055bcb0f4fcb19fb116d7b16a8fbc59bbeca
PedroSpelta <p.h.s.8989@hotmail.com>
2021-07-25 23:18:57 -0300

    parte de informacao primaria do modal pronta

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './styles.css';
 import './pokemon.css';
 import './modal.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import pokemons from './data';
 import element from './elements';
 import pokeball from './images/pokeball.png';
+import filter from './images/filter.png'
 
 function Pokemon(props) {
-  const { name } = props;
+  const { name, showModal, setActualPokemon } = props;
   const [pokeInfo, setPokeInfo] = useState({});
   const [isActive, setIsActive] = useState(true);
   useEffect(() => {
@@ -24,6 +25,7 @@ function Pokemon(props) {
           // const spirte =
           // const local = localStorage.setItem()
           setPokeInfo({
+            name,
             id: json.id,
             sprite: gif,
             type: json.types[0].type.name,
@@ -35,36 +37,48 @@ function Pokemon(props) {
   });
   return (
     <div
-      className="pokemon-container"
-      style={{ backgroundColor: element[pokeInfo.type] }}
+      onClick={() => {
+        setActualPokemon(pokeInfo);
+        showModal();
+      }}
     >
-      <img className="poke-ball" src={pokeball} alt="test" />
-      <p className="pokemon-id"># {pokeInfo.id}</p>
-      <div className="pokemon-info">
-        <p className="pokemon-name">
-          {name[0].toUpperCase()}
-          {name.slice(1)}
-        </p>
-        <p className="pokemon-type">{pokeInfo.type}</p>
-        <p className="pokemon-type">{pokeInfo.type2}</p>
-        <p>{pokeInfo.weight} kg</p>
-      </div>
-      <div className="pokemon-gif">
-        <img src={pokeInfo.sprite} alt={`${name} animated gif`} />
+      <div
+        className="pokemon-container"
+        style={{ backgroundColor: element[pokeInfo.type] }}
+      >
+        <img className="poke-ball" src={pokeball} alt="test" />
+        <p className="pokemon-id"># {pokeInfo.id}</p>
+        <div className="pokemon-info">
+          <p className="pokemon-name">
+            {name[0].toUpperCase()}
+            {name.slice(1)}
+          </p>
+          <p className="pokemon-type">{pokeInfo.type}</p>
+          <p className="pokemon-type">{pokeInfo.type2}</p>
+          <p>{pokeInfo.weight} kg</p>
+        </div>
+        <div className="pokemon-gif">
+          <img src={pokeInfo.sprite} alt={`${name} animated gif`} />
+        </div>
       </div>
     </div>
   );
 }
 
-function Pokedex() {
+function Pokedex({ showModal, setActualPokemon }) {
   // const [pokeList, setPokeList] = useState([]);
   // const [audio] = useState(new Audio('/backgroundmsc.mp3'))
+  function click() {
+    console.log('eae');
+  }
   useEffect(() => {
     // audio.play();
   });
   return pokemons.map((pokemon) => (
     <div>
       <Pokemon
+        setActualPokemon={setActualPokemon}
+        showModal={showModal}
         name={pokemon.pokemon_species.name}
         key={pokemon.pokemon_species.name}
       />
@@ -72,26 +86,61 @@ function Pokedex() {
   ));
 }
 
-function Modal({showModal, setShowModal}) {
-  return (showModal 
-  ? 
-  <div className='modal-background'>
-    <h1>modal text</h1> 
-  </div>
-  : null
-)
+function Modal({ showModal, setShowModal, actualPokemon }) {
+  const modalRef = useRef();
+
+  const closeModal = (e) => {
+    if (modalRef.current === e.target) {
+      setShowModal(false);
+    }
+  };
+  return showModal ? (
+    <div className="modal-background" ref={modalRef} onClick={closeModal}>
+      <div className="modal-div">
+        <div className="modal-pokemon-visual-black">
+          <div className="modal-pokemon-visual">
+            <img
+              className="modal-pokemon-img"
+              src={`https://cdn.traction.one/pokedex/pokemon/${actualPokemon.id}.png`}
+              alt=""
+            />
+            <img src={filter} className='filter' alt="filtro" />
+            <div className="modal-pokemon-visual-info">
+              <h1 className="modal-pokemon-name">
+                {actualPokemon.name.toUpperCase()}
+              </h1>
+              <p
+                className="modal-pokemon-type"
+                style={{ backgroundColor: element[actualPokemon.type] }}
+              >
+                {actualPokemon.type.toUpperCase()}
+              </p>
+              <p className="modal-pokemon-id">{actualPokemon.id}</p>
+            </div>
+          </div>
+          <div className="modal-pokemon-status">
+            <p>atk def etc</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : null;
 }
 
 export default function App() {
   const [showModal, setShowModal] = useState(false);
+  const [actualPokemon, setActualPokemon] = useState({ id: 1 });
   const openModal = () => {
     setShowModal((prev) => !prev);
   };
   return (
     <div className="pokedex-container">
-      <Pokedex />
-      <Modal showModal={showModal} setShowModal={setShowModal} />
-      <button onClick={openModal}>show modal</button>
+      <Pokedex showModal={openModal} setActualPokemon={setActualPokemon} />
+      <Modal
+        actualPokemon={actualPokemon}
+        showModal={showModal}
+        setShowModal={setShowModal}
+      />
     </div>
   );
 }
